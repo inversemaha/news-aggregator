b6e9205e8cd6f5018efa9c1c303301383dcfa167
inversemaha <mahajur88@gmail.com>
2024-11-14 18:00:26 +0600

    Define API routes for authentication and article management

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('v1/api')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::middleware('auth:sanctum')->group(function (Request $request) {
+        Route::get('/articles', [ArticleController::class, 'index']);
+        Route::get('/articles/{id}', [ArticleController::class, 'show']);
+        Route::post('/preferences', [UserPreferenceController::class, 'store']);
+        Route::get('/personalized-feed', [ArticleController::class, 'personalizedFeed']);
+    });
+
 });
